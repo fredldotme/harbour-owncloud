37128ad9d3cc6ba9e622739ffb6bee51107d3145
Matija Šuklje <matija@suklje.name>
2015-11-12 11:36:20 +0100

    Add Slovenian translation of the app
    There are one or two fuzzies, but in general it’s fine and a very good start.

---
+++ app/translations/harbour-owncloud-sl.ts
@@ -0,0 +1,50 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="sl_SI" sourcelanguage="en">
+<context>
+    <name>FileBrowser</name>
+    <message>
+        <source>Refresh</source>
+        <translation>Ponovno naloži</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Nastavitve</translation>
+    </message>
+    <message>
+        <source>File transfers</source>
+        <translation>Prenosi datotek</translation>
+    </message>
+    <message>
+        <source>Upload</source>
+        <translation>Naloži</translation>
+    </message>
+    <message>
+        <source>Create directory</source>
+        <translation>Ustvari mapo</translation>
+    </message>
+</context>
+<context>
+    <name>FileDetails</name>
+    <message>
+        <source>Download</source>
+        <translation>Prenesi</translation>
+    </message>
+    <message>
+        <source>Download and open</source>
+        <translation>Prenesi in odpri</translation>
+    </message>
+    <message>
+        <source>Details</source>
+        <translation>Podrobnosti</translation>
+    </message>
+</context>
+<context>
+    <name>TransferPage</name>
+    <message>
+        <source>No pending file transfers</source>
+        <translatorcomment>Not superb, but close to LUGOS terminology.</translatorcomment>
+        <translation>Ni prenosov na čakanju</translation>
+    </message>
+</context>
+</TS>

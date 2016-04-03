98586915341721566021837e8838135f2fe043a7
Hauke Schade <cnlpete@cnlpete.de>
2016-04-03 11:30:29 +0200

    added german translation texts

--- app/translations/harbour-owncloud-de.ts
+++ app/translations/harbour-owncloud-de.ts
@@ -1,252 +1,252 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="de_DE">
 <context>
     <name>FileBrowser</name>
     <message>
         <source>Refresh</source>
-        <translation type="unfinished"></translation>
+        <translation>Aktualisieren</translation>
     </message>
     <message>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Einstellungen</translation>
     </message>
     <message>
         <source>File transfers</source>
-        <translation type="unfinished"></translation>
+        <translation>Übertragungen</translation>
     </message>
     <message>
         <source>Upload</source>
-        <translation type="unfinished"></translation>
+        <translation>Hochladen</translation>
     </message>
     <message>
         <source>Create directory</source>
-        <translation type="unfinished"></translation>
+        <translation>Ordner erstellen</translation>
     </message>
     <message>
         <source>Delete</source>
-        <translation type="unfinished"></translation>
+        <translation>Löschen</translation>
     </message>
     <message>
         <source>Deleting</source>
         <comment>RemorseItem text</comment>
-        <translation type="unfinished"></translation>
+        <translation>Löschen</translation>
     </message>
 </context>
 <context>
     <name>FileDetails</name>
     <message>
         <source>Download</source>
-        <translation type="unfinished"></translation>
+        <translation>Herunterladen</translation>
     </message>
     <message>
         <source>Download and open</source>
-        <translation type="unfinished"></translation>
+        <translation>Herunterladen und Öffnen</translation>
     </message>
     <message>
         <source>Details</source>
-        <translation type="unfinished"></translation>
+        <translation>Details</translation>
     </message>
     <message>
         <source>File name:</source>
-        <translation type="unfinished"></translation>
+        <translation>Dateiname:</translation>
     </message>
     <message>
         <source>Size:</source>
-        <translation type="unfinished"></translation>
+        <translation>Größe:</translation>
     </message>
     <message>
         <source>Last modified:</source>
-        <translation type="unfinished"></translation>
+        <translation>Zuletzt verändert:</translation>
     </message>
     <message>
         <source>Type:</source>
-        <translation type="unfinished"></translation>
+        <translation>Typ:</translation>
     </message>
 </context>
 <context>
     <name>Login</name>
     <message>
         <source>Host address</source>
-        <translation type="unfinished"></translation>
+        <translation>Serveradresse</translation>
     </message>
     <message>
         <source>User name</source>
-        <translation type="unfinished"></translation>
+        <translation>Benutzername</translation>
     </message>
     <message>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Passwort</translation>
     </message>
     <message>
         <source>Login automatically</source>
-        <translation type="unfinished"></translation>
+        <translation>Automatisch anmelden</translation>
     </message>
     <message>
         <source>Accept certificate</source>
-        <translation type="unfinished"></translation>
+        <translation>Zertifikat akzeptieren</translation>
     </message>
     <message>
         <source>Continue</source>
-        <translation type="unfinished"></translation>
+        <translation>Weiter</translation>
     </message>
     <message>
         <source>Invalid URL</source>
-        <translation type="unfinished"></translation>
+        <translation>Falsche URL</translation>
     </message>
     <message>
         <source>Please check your host address</source>
-        <translation type="unfinished"></translation>
+        <translation>Bitte überprüfe deine Serveradresse</translation>
     </message>
 </context>
 <context>
     <name>MkDirDialog</name>
     <message>
         <source>Accept</source>
-        <translation type="unfinished"></translation>
+        <translation>Aktzeptieren</translation>
     </message>
     <message>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Abbrechen</translation>
     </message>
     <message>
         <source>Directory name</source>
-        <translation type="unfinished"></translation>
+        <translation>Ordnerpfad</translation>
     </message>
 </context>
 <context>
     <name>SSLErrorDialog</name>
     <message>
         <source>Do you want to accept this certificate?</source>
-        <translation type="unfinished"></translation>
+        <translation>Möchtest du dieses Zertifikat akzeptieren?</translation>
     </message>
     <message>
         <source>MD5 fingerprint:</source>
-        <translation type="unfinished"></translation>
+        <translation>MD5 Fingerprint:</translation>
     </message>
     <message>
         <source>SHA1 fingerprint:</source>
-        <translation type="unfinished"></translation>
+        <translation>SHA1 Fingerprint:</translation>
     </message>
 </context>
 <context>
     <name>SettingsPage</name>
     <message>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Einstellungen</translation>
     </message>
     <message>
         <source>Reset connection settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Verbindungseinstellungen zurücksetzen</translation>
     </message>
     <message>
         <source>Login automatically</source>
-        <translation type="unfinished"></translation>
+        <translation>Automatisch anmelden</translation>
     </message>
     <message>
         <source>Automatically log in to your ownCloud server when starting the app</source>
         <comment>Login automatically description</comment>
-        <translation type="unfinished"></translation>
+        <translation>Automatisch anmelden, wenn die Anwendung startet</translation>
     </message>
     <message>
         <source>Notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Benachrichtigungen</translation>
     </message>
     <message>
         <source>Show global notifications when transfering files</source>
         <comment>Notifications description</comment>
-        <translation type="unfinished"></translation>
+        <translation>Zeige globale Benachrichtigungen when Dateiübertragungen stattfinden</translation>
     </message>
     <message>
         <source>Camera photo backups</source>
-        <translation type="unfinished"></translation>
+        <translation>Kamera Backup</translation>
     </message>
     <message>
         <source>Automatically save camera photos to your ownCloud instance when on WiFi</source>
         <comment>Camera photo backups escription</comment>
-        <translation type="unfinished"></translation>
+        <translation>Speichere die Kamerabilder automatisch in deiner ownCloud when du im WLAN bist</translation>
     </message>
     <message>
         <source>Photo backups via mobile internet connection</source>
-        <translation type="unfinished"></translation>
+        <translation>Kamera Backup über Mobile Daten</translation>
     </message>
     <message>
         <source>Also automatically backup camera photos when connected via 2G, 3G or LTE</source>
         <comment>hoto backups via mobile internet connection description</comment>
-        <translation type="unfinished"></translation>
+        <translation>Speichere deine Kamerabilder automatisch wenn du eine mobile Verbindung hast (2G, 3G, LTE)</translation>
     </message>
 </context>
 <context>
     <name>TransferPage</name>
     <message>
         <source>No pending file transfers</source>
-        <translation type="unfinished"></translation>
+        <translation>Keine ausstehenden Dateiübertragungen</translation>
     </message>
     <message>
         <source>File transfers</source>
-        <translation type="unfinished"></translation>
+        <translation>Übertragungen</translation>
     </message>
     <message>
         <source>Camera backup in progress...</source>
-        <translation type="unfinished"></translation>
+        <translation>Kamera Backup läuft...</translation>
     </message>
     <message>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Abbrechen</translation>
     </message>
 </context>
 <context>
     <name>UploadDialog</name>
     <message>
         <source>Upload</source>
-        <translation type="unfinished"></translation>
+        <translation>Hochladen</translation>
     </message>
     <message>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Abbrechen</translation>
     </message>
 </context>
 <context>
     <name>harbour-owncloud</name>
     <message>
         <source>Login failed</source>
-        <translation type="unfinished"></translation>
+        <translation>Anmelden fehlgeschlagen</translation>
     </message>
     <message>
         <source>Please check your host address and credentials</source>
-        <translation type="unfinished"></translation>
+        <translation>Bitte prüfe die Serveradresse und die Anmelde-Credentials</translation>
     </message>
     <message>
         <source>Upload complete</source>
-        <translation type="unfinished"></translation>
+        <translation>Hochladen vollständig</translation>
     </message>
     <message>
         <source>%1 uploaded successfully</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 wurde erfolgreich hochgeladen</translation>
     </message>
     <message>
         <source>Download complete</source>
-        <translation type="unfinished"></translation>
+        <translation>Herunterladen vollständig</translation>
     </message>
     <message>
         <source>%1 downloaded successfully</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 wurde erfolgreich heruntergeladen</translation>
     </message>
     <message>
         <source>Upload failed!</source>
-        <translation type="unfinished"></translation>
+        <translation>Hochladen fehlgeschlagen!</translation>
     </message>
     <message>
         <source>%1 couldn&apos;t be uploaded</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 konnte nicht hochgeladen werden</translation>
     </message>
     <message>
         <source>Download failed!</source>
-        <translation type="unfinished"></translation>
+        <translation>Herunterladen fehlgeschlagen!</translation>
     </message>
     <message>
         <source>%1 couldn&apos;t be downloaded</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 konnte nicht heruntergeladen werden</translation>
     </message>
 </context>
 </TS>

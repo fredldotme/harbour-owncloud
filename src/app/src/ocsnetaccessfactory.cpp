8b3a704245c574018321f2fa269aee402e2d7788
Alfred Neumayer <dev.beidl@gmail.com>
2018-07-29 09:30:00 +0200

    app: keep Url interception code around but commented out

--- src/app/src/ocsnetaccessfactory.cpp
+++ src/app/src/ocsnetaccessfactory.cpp
@@ -1,29 +1,58 @@
 #include "ocsnetaccessfactory.h"
 
 #include <QDebug>
+#include <QNetworkDiskCache>
+#include <QStandardPaths>
+#include <net/webdav_utils.h>
 
-OscNetAccess::OscNetAccess(QObject *parent) : QNetworkAccessManager(parent)
+OscNetAccess::OscNetAccess(QObject* parent, NextcloudSettingsBase* settings) :
+    QNetworkAccessManager(parent), m_settings(settings)
+{ }
+
+QNetworkReply* OscNetAccess::createRequest(Operation op,
+                                           const QNetworkRequest &request,
+                                           QIODevice *outgoingData)
 {
-
+    qDebug() << Q_FUNC_INFO << this->m_settings;
+    return QNetworkAccessManager::createRequest(op,
+                                                getOcsRequest(request, this->m_settings),
+                                                outgoingData);
 }
 
-QNetworkRequest getOcsRequest(const QNetworkRequest& request)
+OcsNetAccessFactory::OcsNetAccessFactory(NextcloudSettingsBase *settings)
+    : m_settings(settings)
 {
-    QNetworkRequest ret(request);
-    ret.setRawHeader(QByteArrayLiteral("OCS-APIREQUEST"),
-                     QByteArrayLiteral("true"));
-    qDebug() << "getOcsRequest()";
-    return ret;
+    qDebug() << Q_FUNC_INFO;
 }
 
-QNetworkReply* OscNetAccess::createRequest(Operation op, const QNetworkRequest &request, QIODevice *outgoingData)
+OcsNetAccessFactory::OcsNetAccessFactory(const OcsNetAccessFactory &o)
 {
-    return QNetworkAccessManager::createRequest(op, getOcsRequest(request), outgoingData);
+    qDebug() << Q_FUNC_INFO;
+    this->m_settings = o.m_settings;
 }
 
 QNetworkAccessManager* OcsNetAccessFactory::create(QObject *parent)
 {
     qDebug() << Q_FUNC_INFO;
-    OscNetAccess *nam = new OscNetAccess(parent);
+
+    OscNetAccess *nam = new OscNetAccess(parent, this->m_settings);
+    QNetworkDiskCache* cache = new QNetworkDiskCache(nam);
+    cache->setCacheDirectory(QStandardPaths::writableLocation(QStandardPaths::CacheLocation));
+    nam->setCache(cache);
     return nam;
 }
+
+/*QUrl OcsNetAccessFactory::intercept(const QUrl& url, DataType type)
+{
+
+    if (type != QQmlAbstractUrlInterceptor::UrlString)
+        return url;
+
+    qDebug() << Q_FUNC_INFO << url.scheme() << url;
+
+    if (!(url.scheme().startsWith(QStringLiteral("http://")) ||
+          url.scheme().startsWith(QStringLiteral("https://"))))
+        return url;
+
+    return url;
+}*/
--- src/app/src/ocsnetaccessfactory.h
+++ src/app/src/ocsnetaccessfactory.h
@@ -7,19 +7,39 @@
 #include <QNetworkReply>
 #include <QNetworkRequest>
 
+#include <QQmlAbstractUrlInterceptor>
+
+#include <settings/nextcloudsettingsbase.h>
+
 class OscNetAccess : public QNetworkAccessManager
 {
+    Q_OBJECT
 public:
-    explicit OscNetAccess(QObject* parent = Q_NULLPTR);
+    explicit OscNetAccess(QObject* parent = Q_NULLPTR,
+                          NextcloudSettingsBase* settings = Q_NULLPTR);
 
-protected:
-    virtual QNetworkReply* createRequest(Operation op, const QNetworkRequest &request, QIODevice* outgoingData);
+private:
+    NextcloudSettingsBase* m_settings = Q_NULLPTR;
+
+public:
+    virtual QNetworkReply* createRequest(Operation op,
+                                         const QNetworkRequest &request,
+                                         QIODevice* outgoingData) Q_DECL_OVERRIDE;
 };
 
-class OcsNetAccessFactory : public QQmlNetworkAccessManagerFactory
+class OcsNetAccessFactory :
+        //public QQmlAbstractUrlInterceptor,
+        public QQmlNetworkAccessManagerFactory
 {
 public:
+    OcsNetAccessFactory(NextcloudSettingsBase* settings = Q_NULLPTR);
+    OcsNetAccessFactory(const OcsNetAccessFactory& o);
+
+    //QUrl intercept(const QUrl& url, DataType type) Q_DECL_OVERRIDE;
     QNetworkAccessManager* create(QObject *parent) Q_DECL_OVERRIDE;
+
+private:
+    NextcloudSettingsBase* m_settings = Q_NULLPTR;
 };
 
 

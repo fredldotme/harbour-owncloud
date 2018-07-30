da620cc315b4d6722e2ded7333d728ac83ddd6dc
Alfred Neumayer <dev.beidl@gmail.com>
2018-07-30 12:13:25 +0200

    common: add avatar request endpoint

--- src/common/src/nextcloudendpointconsts.h
+++ src/common/src/nextcloudendpointconsts.h
@@ -7,6 +7,7 @@
 const QString NEXTCLOUD_ENDPOINT_WEBDAV = QStringLiteral("remote.php/webdav");
 const QString NEXTCLOUD_ENDPOINT_LOGIN_FLOW = QStringLiteral("index.php/login/flow");
 const QString NEXTCLOUD_ENDPOINT_THUMBNAIL = QStringLiteral("index.php/apps/files/api/v1/thumbnail");
+const QString NEXTCLOUD_ENDPOINT_AVATAR = QStringLiteral("index.php/avatar/%1/%2");
 const QString NEXTCLOUD_ENDPOINT_OCS_USERINFO = QStringLiteral("ocs/v2.php/cloud/users/");
 
 /*class NextcloudApiEndpoints {

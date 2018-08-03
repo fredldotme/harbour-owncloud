7ea1fc7b907905ac9597dbedb2fdec6af993a17e
Alfred Neumayer <dev.beidl@gmail.com>
2018-08-03 09:10:19 +0200

    app: watch for daemon appearance and disappearance and support the new abort() method

--- src/app/src/daemoncontrol.cpp
+++ src/app/src/daemoncontrol.cpp
@@ -4,13 +4,41 @@
 
 DaemonControl::DaemonControl(QObject *parent) : QObject(parent)
 {
-    m_uploading = false;
-    QDBusConnection::sessionBus().connect(HarbourOwncloud::DBusConsts::DBUS_SERVICE,
-                                          HarbourOwncloud::DBusConsts::DBUS_PATH,
-                                          HarbourOwncloud::DBusConsts::DBUS_INTERFACE,
-                                          HarbourOwncloud::DBusConsts::DBUS_SIGNAL_UPLOADINGCHANGED,
-                                          this,
-                                          SLOT(setUploading(bool)));
+    this->m_uploading = false;
+
+    QDBusServiceWatcher::WatchMode daemonWatchFlags =
+            (QDBusServiceWatcher::WatchForRegistration |
+             QDBusServiceWatcher::WatchForUnregistration);
+
+    this->m_daemonWatcher =
+            new QDBusServiceWatcher(HarbourOwncloud::DBusConsts::DBUS_SERVICE,
+                                    QDBusConnection::sessionBus(),
+                                    daemonWatchFlags,
+                                    this);
+
+    QObject::connect(this->m_daemonWatcher, &QDBusServiceWatcher::serviceRegistered,
+                     this, &DaemonControl::daemonRegistered);
+    QObject::connect(this->m_daemonWatcher, &QDBusServiceWatcher::serviceUnregistered,
+                     this, &DaemonControl::daemonUnregistered);
+
+
+    this->m_daemonInterface =
+            new QDBusInterface(HarbourOwncloud::DBusConsts::DBUS_SERVICE,
+                               HarbourOwncloud::DBusConsts::DBUS_PATH,
+                               HarbourOwncloud::DBusConsts::DBUS_INTERFACE,
+                               QDBusConnection::sessionBus(),
+                               this);
+    this->m_daemonInterface->setTimeout(1000);
+
+    setUploading(isCurrentlyUploading());
+
+    QDBusConnection::sessionBus()
+            .connect(HarbourOwncloud::DBusConsts::DBUS_SERVICE,
+                     HarbourOwncloud::DBusConsts::DBUS_PATH,
+                     HarbourOwncloud::DBusConsts::DBUS_INTERFACE,
+                     HarbourOwncloud::DBusConsts::DBUS_SIGNAL_UPLOADINGCHANGED,
+                     this,
+                     SLOT(setUploading(bool)));
 }
 
 bool DaemonControl::daemonInstalled()
@@ -20,14 +48,14 @@ bool DaemonControl::daemonInstalled()
 
 bool DaemonControl::uploading()
 {
-    return m_uploading;
+    return this->m_uploading;
 }
 
 void DaemonControl::setUploading(bool value)
 {
     qDebug() << "Uploading? " << value;
-    if(value != m_uploading) {
-        m_uploading = value;
+    if(value != this->m_uploading) {
+        this->m_uploading = value;
         emit uploadingChanged();
     }
 }
@@ -37,9 +65,36 @@ void DaemonControl::reloadConfig()
     if (!daemonInstalled())
         return;
 
-    QProcess restartCmd;
-    restartCmd.setProgram(QStringLiteral("/bin/systemctl"));
-    restartCmd.setArguments(QStringList() << "--user" << "restart" << "harbour-owncloud-daemon");
-    restartCmd.start();
-    restartCmd.waitForFinished(3000);
+    this->m_daemonInterface->
+            asyncCall(HarbourOwncloud::DBusConsts::DBUS_METHOD_RELOADCONFIG);
+}
+
+void DaemonControl::abort()
+{
+    if (!daemonInstalled())
+        return;
+
+    this->m_daemonInterface->
+            asyncCall(HarbourOwncloud::DBusConsts::DBUS_METHOD_ABORT);
+}
+
+void DaemonControl::daemonRegistered(const QString &serviceName)
+{
+    qDebug() << serviceName;
+
+    setUploading(isCurrentlyUploading());
+}
+
+void DaemonControl::daemonUnregistered(const QString &serviceName)
+{
+    qDebug() << serviceName;
+    setUploading(false);
+}
+
+bool DaemonControl::isCurrentlyUploading()
+{
+    const QVariant currentlyUploading =
+            this->m_daemonInterface
+            ->property(HarbourOwncloud::DBusConsts::DBUS_PROPERTY_UPLOADING.toUtf8().data());
+    return (currentlyUploading.isValid() && currentlyUploading.toBool());
 }
--- src/app/src/daemoncontrol.h
+++ src/app/src/daemoncontrol.h
@@ -14,13 +14,21 @@ public:
     Q_PROPERTY(bool daemonInstalled READ daemonInstalled NOTIFY daemonInstalledChanged)
     Q_PROPERTY(bool uploading READ uploading NOTIFY uploadingChanged)
 
-    Q_INVOKABLE void reloadConfig();
-
-private:
-    bool m_uploading;
-
     bool daemonInstalled();
     bool uploading();
+
+public slots:
+    void reloadConfig();
+    void abort();
+
+private:
+    void daemonRegistered(const QString& serviceName);
+    void daemonUnregistered(const QString& serviceName);
+    bool isCurrentlyUploading();
+
+    bool m_uploading;
+    QDBusInterface* m_daemonInterface = Q_NULLPTR;
+    QDBusServiceWatcher* m_daemonWatcher = Q_NULLPTR;
 
 signals:
     void daemonInstalledChanged();

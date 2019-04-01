bae99a0dbe6bc9d1f15b8bfbd6426e78fc8633d7
Alfred Neumayer <dev.beidl@gmail.com>
2019-04-01 15:44:45 +0200

    Revert "click: Use the Material theme for now"
    This reverts commit e3523212d3bda5155c19bf4f1ac0d900e884000e.

--- click/run.sh
+++ click/run.sh
@@ -2,7 +2,6 @@
 
 export LD_LIBRARY_PATH=$PWD/usr/lib:$LD_LIBRARY_PATH
 export QT_QUICK_CONTROLS_MOBILE=true
-#export QT_QUICK_CONTROLS_STYLE=Suru
-export QT_QUICK_CONTROLS_STYLE=Material
+export QT_QUICK_CONTROLS_STYLE=Suru
 
 $PWD/usr/bin/harbour-owncloud

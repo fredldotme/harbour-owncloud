efb0de899632f226f375baca56b7443b34c00050
Alfred Neumayer <dev.beidl@gmail.com>
2019-05-01 16:17:35 +0200

    click: Use Material style

--- click/run.sh
+++ click/run.sh
@@ -2,6 +2,6 @@
 
 export LD_LIBRARY_PATH=$PWD/usr/lib:$LD_LIBRARY_PATH
 export QT_QUICK_CONTROLS_MOBILE=true
-export QT_QUICK_CONTROLS_STYLE=Suru
+export QT_QUICK_CONTROLS_STYLE=Material
 
 $PWD/usr/bin/harbour-owncloud

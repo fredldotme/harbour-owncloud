519076e89266349eecc04ae0b86f03b12903bac0
Matti Viljanen <matti.viljanen@kapsi.fi>
2020-04-13 12:21:05 +0300

    Fix broken xml

--- src/app/translations/harbour-owncloud-zh_CN.ts
+++ src/app/translations/harbour-owncloud-zh_CN.ts
@@ -1,1186 +1,599 @@
 <?xml version="1.0" encoding="utf-8"?>
-
 <!DOCTYPE TS>
-
 <TS version="2.1">
-
 <context>
-
     <name>About</name>
-
     <message>
-
         <source>Donation</source>
-
         <translation>捐赠</translation>
-
     </message>
-
     <message>
-
         <source>Copyright notices</source>
-
         <translation>版权提示</translation>
-
     </message>
-
     <message>
-
         <source>QWebDAV library (qwebdavlib) version 1.0 (LGPL v2.1)</source>
-
         <translation>QWebDAV l库 (qwebdavlib) 版本 1.0 (LGPL v2.1)</translation>
-
     </message>
-
     <message>
-
         <source>Licensed under GPL v2</source>
-
         <translation>使用 GPL v2 许可协议</translation>
-
     </message>
-
     <message>
-
         <source>Source code on GitHub</source>
-
         <translation>源代码位于 Github</translation>
-
     </message>
-
     <message>
-
         <source>Via PayPal</source>
-
         <translation>通过 Paypal</translation>
-
     </message>
-
     <message>
-
         <source>Via Flattr</source>
-
         <translation>通过 Flattr</translation>
-
     </message>
-
     <message>
-
         <source>SailfishUiSet UI components (GPL v2)</source>
-
         <translation>SailfishUiSet UI 组件(GPL v2)</translation>
-
     </message>
-
     <message>
-
         <source>About GhostCloud</source>
-
         <translation>关于 GhostCloud</translation>
-
     </message>
-
     <message>
-
         <source>GhostCloud</source>
-
         <translation>GhostCloud</translation>
-
     </message>
-
     <message>
-
         <source>Icon based on &quot;Cute clouds for CSS sprites&quot; by Yug (CC BY-SA 3.0)</source>
-
         <translation>图标基于 Yug 制作的可爱的 CSS 云精灵(CC BY-SA 3.0)</translation>
-
     </message>
-
     <message>
-
         <source>Easily access your Nextcloud, ownCloud and WebDav instances with a native Free Software application</source>
-
         <translation>通过一个原生的自由软件，让你更轻松地访问你的 Nextcloud、ownCloud 及 WebDav 实例。</translation>
-
     </message>
-
     <message>
-
         <source>Easily access your Nextcloud, ownCloud and WebDav instances with a native, multi-platform Free Software application</source>
-
         <translation>通过一个原生的多平台自由软件，让你更轻松地访问你的 Nextcloud、ownCloud 及 WebDav 实例。</translation>
-
     </message>
-
     <message>
-
         <source>QmlUiSet UI components (LGPL v2.1)</source>
-
-        <translation>QmlUiSet UI 组件 (LGPL v2.1)<</translation>
-
+        <translation>QmlUiSet UI 组件 (LGPL v2.1)</translation>
     </message>
-
     <message>
-
         <source>KDE Breeze Icon Theme (LGPL v2.1)</source>
-
         <translation>KDE Breeze 图标主题(LGPL v2.1)</translation>
-
     </message>
-
     <message>
-
         <source>libqtcommandqueue (LGPL v2.1)</source>
-
-        <translation>libqtcommandqueue (LGPL v2.1)<</translation>
-
+        <translation>libqtcommandqueue (LGPL v2.1)</translation>
     </message>
-
 </context>
-
 <context>
-
     <name>AccountSelection</name>
-
     <message>
-
         <source>Remove account</source>
-
         <translation>移除账户</translation>
-
     </message>
-
     <message>
-
         <source>Add account...</source>
-
         <translation>正在添加账户……</translation>
-
     </message>
-
     <message>
-
         <source>About</source>
-
         <translation>关于</translation>
-
     </message>
-
     <message>
-
         <source>No accounts available. Please add an account to continue.</source>
-
         <translation>无可用账户，请添加以继续。</translation>
-
     </message>
-
     <message>
-
         <source>Removing account</source>
-
         <translation>正在移除账户</translation>
-
     </message>
-
     <message>
-
         <source>Upload complete</source>
-
         <translation>已更新完成</translation>
-
     </message>
-
     <message>
-
         <source>%1 uploaded successfully</source>
-
         <translation>%1 已成功上传</translation>
-
     </message>
-
     <message>
-
         <source>Upload failed!</source>
-
         <translation>上传失败!</translation>
-
     </message>
-
     <message>
-
         <source>%1 couldn&apos;t be uploaded</source>
-
         <translation>%1 无法上传</translation>
-
     </message>
-
     <message>
-
         <source>Download complete</source>
-
         <translation>下载完成</translation>
-
     </message>
-
     <message>
-
         <source>%1 downloaded successfully</source>
-
         <translation>%1 下载成功</translation>
-
     </message>
-
     <message>
-
         <source>Download failed!</source>
-
         <translation>下载失败!</translation>
-
     </message>
-
     <message>
-
         <source>%1 couldn&apos;t be downloaded</source>
-
         <translation>%1 无法下载</translation>
-
     </message>
-
     <message>
-
         <source>Settings</source>
-
         <translation>设置</translation>
-
     </message>
-
     <message>
-
         <source>Settings...</source>
-
         <translation>设置中……</translation>
-
     </message>
-
+    <message>
+        <source>%1 on %2</source>
+        <comment>username on https://server:443/dav/</comment>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
-
 <context>
-
     <name>BasicAuthentication</name>
-
     <message>
-
         <source>Host address</source>
-
         <translation>主机地址</translation>
-
     </message>
-
     <message>
-
         <source>Username</source>
-
         <translation>用户名</translation>
-
     </message>
-
     <message>
-
         <source>Password</source>
-
         <translation>密码</translation>
-
     </message>
-
     <message>
-
         <source>Accept certificate</source>
-
         <translation>接受凭证</translation>
-
     </message>
-
     <message>
-
         <source>Continue</source>
-
         <translation>继续</translation>
-
     </message>
-
     <message>
-
         <source>Invalid URL</source>
-
         <translation>无效 URL</translation>
-
     </message>
-
     <message>
-
         <source>Please check your host address</source>
-
         <translation>请核对你的主机地址</translation>
-
     </message>
-
     <message>
-
         <source>Login failed</source>
-
         <translation>登录失败</translation>
-
     </message>
-
     <message>
-
         <source>Please check your connection or try again later.</source>
-
         <translation>请检查你的连接或重试</translation>
-
     </message>
-
     <message>
-
         <source>Provider:</source>
-
         <translation>提供者：</translation>
-
     </message>
-
     <message>
-
         <source>Nextcloud/ownCloud</source>
-
-        <translation>Nextcloud/ownCloud<</translation>
-
+        <translation>Nextcloud/ownCloud</translation>
     </message>
-
     <message>
-
         <source>WebDav</source>
-
         <translation>WebDav</translation>
-
     </message>
-
     <message>
-
         <source>About</source>
-
         <translation>关于</translation>
-
     </message>
-
 </context>
-
 <context>
-
     <name>BrowserCommandPageFlow</name>
-
     <message>
-
         <source>Failed to get remote content</source>
-
         <translation>获取远程内容失败</translation>
-
     </message>
-
     <message>
-
         <source>Please check your connection or try again later.</source>
-
         <translation>请检查你的连接或重试</translation>
-
     </message>
-
     <message>
-
         <source>Operation failed</source>
-
         <translation>操作失败</translation>
-
     </message>
-
     <message>
-
         <source>Would you like to remove the existing file &apos;%1&apos; before starting the download?</source>
-
         <translation>你是否想在开始之前移除已存在的 %1 吗
 
 ？</translation>
-
     </message>
-
     <message>
-
         <source>Error occured</source>
-
         <translation>出现错误</translation>
-
     </message>
-
     <message>
-
         <source>Please check your credentials or try again later.</source>
-
         <translation>请核对你的凭证或重试。</translation>
-
     </message>
-
 </context>
-
 <context>
-
     <name>CoverPage</name>
-
     <message>
-
         <source>User:</source>
-
         <translation>用户名：</translation>
-
     </message>
-
     <message>
-
         <source>Usage:</source>
-
         <translation>使用：</translation>
-
     </message>
-
     <message>
-
         <source>Total:</source>
-
         <translation>总的：</translation>
-
     </message>
-
 </context>
-
 <context>
-
     <name>Entrance</name>
-
     <message>
-
         <source></source>
-
         <translation></translation>
-
     </message>
-
     <message>
-
         <source>Host address</source>
-
         <translation>主机名</translation>
-
     </message>
-
     <message>
-
         <source>Continue</source>
-
         <translation>继续</translation>
-
     </message>
-
     <message>
-
         <source>Manual configuration</source>
-
         <translation>手动配置</translation>
-
     </message>
-
 </context>
-
 <context>
-
     <name>FileBrowser</name>
-
     <message>
-
         <source>Refresh</source>
-
         <translation>刷新</translation>
-
     </message>
-
     <message>
-
         <source>File transfers</source>
-
         <translation>文件传输</translation>
-
     </message>
-
     <message>
-
         <source>Upload</source>
-
         <translation>上传</translation>
-
     </message>
-
     <message>
-
         <source>Create directory</source>
-
         <translation>创建目录</translation>
-
     </message>
-
     <message>
-
         <source>Directory name</source>
-
         <translation>目录名称</translation>
-
     </message>
-
     <message>
-
         <source>User:</source>
-
         <translation>用户：</translation>
-
     </message>
-
     <message>
-
         <source>Mail:</source>
-
         <translation>邮件：</translation>
-
     </message>
-
     <message>
-
         <source>Usage:</source>
-
         <translation>使用：</translation>
-
     </message>
-
     <message>
-
         <source>Total:</source>
-
         <translation>总的：</translation>
-
     </message>
-
     <message>
-
         <source>About</source>
-
         <translation>关于</translation>
-
     </message>
-
     <message>
-
         <source>Folder is empty</source>
-
         <translation>文件夹为空</translation>
-
     </message>
-
     <message>
-
         <source>Enter directory name:</source>
-
         <translation>输入文件夹名称</translation>
-
     </message>
-
     <message>
-
         <source>Enter new name:</source>
-
         <translation>输入新名称：</translation>
-
     </message>
-
     <message>
-
         <source>Free:</source>
-
         <translation>闲置：</translation>
-
     </message>
-
     <message>
-
         <source>Open</source>
-
         <translation>打开</translation>
-
     </message>
-
     <message>
-
         <source>Download</source>
-
         <translation>下载</translation>
-
     </message>
-
     <message>
-
         <source>Rename</source>
-
         <translation>重命名</translation>
-
     </message>
-
     <message>
-
         <source>Copy</source>
-
         <translation>复制</translation>
-
     </message>
-
     <message>
-
         <source>Move</source>
-
         <translation>移动</translation>
-
     </message>
-
     <message>
-
         <source>Delete</source>
-
         <translation>删除</translation>
-
     </message>
-
     <message>
-
         <source>Details</source>
-
         <translation>详情</translation>
-
     </message>
-
 </context>
-
 <context>
-
     <name>FileDetails</name>
-
     <message>
-
         <source>Download</source>
-
         <translation>下载</translation>
-
     </message>
-
     <message>
-
         <source>Download and open</source>
-
         <translation>下载及打开</translation>
-
     </message>
-
     <message>
-
         <source>Details</source>
-
         <translation>详情</translation>
-
     </message>
-
     <message>
-
         <source>File name:</source>
-
         <translation>文件名：</translation>
-
     </message>
-
     <message>
-
         <source>Size:</source>
-
         <translation>大小：</translation>
-
     </message>
-
     <message>
-
         <source>Last modified:</source>
-
         <translation>最近修改：</translation>
-
     </message>
-
     <message>
-
         <source>Type:</source>
-
         <translation>类型：</translation>
-
     </message>
-
     <message>
-
         <source>Created at:</source>
-
-        <translation><创建于：/translation>
-
+        <translation>创建于：</translation>
     </message>
-
     <message>
-
         <source>Entity tag:</source>
-
         <translation>独立标签：</translation>
-
     </message>
-
     <message>
-
         <source>File ID:</source>
-
         <translation>文件ID:</translation>
-
     </message>
-
     <message>
-
         <source>Directory</source>
-
         <translation>目录</translation>
-
     </message>
-
     <message>
-
         <source>The file &apos;%1&apos; already exists. Would you like to overwrite it?</source>
-
         <translation>文件 %1 已存在。是否想要覆盖?</translation>
-
     </message>
-
     <message>
-
         <source>Open</source>
-
         <translation>打开</translation>
-
     </message>
-
 </context>
-
 <context>
-
     <name>FileOperationsContextMenu</name>
-
     <message>
-
         <source>Rename</source>
-
         <translation>重命名</translation>
-
     </message>
-
     <message>
-
         <source>New name</source>
-
         <translation>新名称</translation>
-
     </message>
-
     <message>
-
         <source>Move</source>
-
         <translation>移动</translation>
-
     </message>
-
     <message>
-
         <source>Copy</source>
-
         <translation>复制</translation>
-
     </message>
-
     <message>
-
         <source>Delete</source>
-
         <translation>删除</translation>
-
     </message>
-
     <message>
-
         <source>Deleting</source>
-
         <comment>RemorseItem text</comment>
-
         <translation>删除中</translation>
-
     </message>
-
     <message>
-
         <source>Details</source>
-
         <translation>详情</translation>
-
     </message>
-
 </context>
-
 <context>
-
     <name>SSLErrorDialog</name>
-
     <message>
-
         <source>Do you want to accept this certificate?</source>
-
         <translation>是否接受此凭证?</translation>
-
     </message>
-
     <message>
-
         <source>MD5 fingerprint:</source>
-
         <translation>MD5 指纹：</translation>
-
     </message>
-
     <message>
-
         <source>SHA1 fingerprint:</source>
-
         <translation>SHA1 指纹：</translation>
-
     </message>
-
     <message>
-
         <source>Would you like to accept the certificate?</source>
-
         <translation>是否想要接受此凭证?</translation>
-
     </message>
-
     <message>
-
         <source>MD5: %1</source>
-
-        <translation>MD5: %1<</translation>
-
+        <translation>MD5: %1</translation>
     </message>
-
     <message>
-
         <source>SHA1: %1</source>
-
         <translation>SHA1: %1</translation>
-
     </message>
-
 </context>
-
 <context>
-
     <name>SettingsPage</name>
-
     <message>
-
         <source>Settings</source>
-
         <translation>设置</translation>
-
     </message>
-
     <message>
-
         <source>Camera photo backups</source>
-
         <translation>备份相机照片</translation>
-
     </message>
-
     <message>
-
         <source>Automatically save camera photos to your ownCloud instance when on WiFi</source>
-
         <comment>Camera photo backups escription</comment>
-
         <translation>当连接 Wifi 时自动备份相机照片到你的 ownCloud 实例。</translation>
-
     </message>
-
     <message>
-
         <source>Photo backups via mobile internet connection</source>
-
         <translation>允许通过移动数据备份照片</translation>
-
     </message>
-
     <message>
-
         <source>Also automatically backup camera photos when connected via 2G, 3G or LTE</source>
-
         <comment>hoto backups via mobile internet connection description</comment>
-
         <translation>通过2G、3G及LTE网络备份相机数据</translation>
-
     </message>
-
     <message>
-
         <source>Clear cache</source>
-
         <translation>清空缓存</translation>
-
     </message>
-
     <message>
-
         <source>Clear downloads</source>
-
         <translation>清空下载</translation>
-
     </message>
-
 </context>
-
 <context>
-
     <name>TransferPage</name>
-
     <message>
-
         <source>No pending file transfers</source>
-
         <translation>没有等待传输的文件</translation>
-
     </message>
-
     <message>
-
         <source>File transfers</source>
-
         <translation>文件传输</translation>
-
     </message>
-
     <message>
-
         <source>Camera backup in progress...</source>
-
         <translation>正在备份相机数据……</translation>
-
     </message>
-
     <message>
-
         <source>Cancel</source>
-
         <translation>取消</translation>
-
     </message>
-
     <message>
-
         <source>Open: &apos;%1&apos;</source>
-
         <translation>打开: &apos;%1&apos;</translation>
-
     </message>
-
 </context>
-
 <context>
-
     <name>UbuntuContentHubOpener</name>
-
     <message>
-
         <source>Open &apos;%1&apos; with</source>
-
         <translation>打开 %1 方式</translation>
-
     </message>
-
 </context>
-
 <context>
-
     <name>WebDavAccountDialog</name>
-
     <message>
-
         <source>Login failed</source>
-
         <translation>登录失败</translation>
-
     </message>
-
     <message>
-
         <source>Please check your connection or try again later.</source>
-
         <translation>请检查你的连接或稍后重试</translation>
-
     </message>
-
     <message>
-
         <source>Host address</source>
-
         <translation>主机地址</translation>
-
     </message>
-
     <message>
-
-        <source>User name</source>
-
-        <translation>用户名</translation>
-
-    </message>
-
-    <message>
-
         <source>Password</source>
-
         <translation>密码</translation>
-
     </message>
-
     <message>
-
         <source>Provider:</source>
-
         <translation>提供者:</translation>
-
     </message>
-
     <message>
-
         <source>Nextcloud/ownCloud</source>
-
         <translation>Nextcloud/ownCloud</translation>
-
     </message>
-
     <message>
-
         <source>WebDav</source>
-
         <translation>WebDav</translation>
-
     </message>
-
     <message>
-
         <source>Accept certificate</source>
-
         <translation>接受凭证</translation>
-
     </message>
-
     <message>
-
         <source>Continue</source>
-
         <translation>继续</translation>
-
     </message>
-
     <message>
-
         <source>Invalid URL</source>
-
         <translation>无效 URL</translation>
-
     </message>
-
     <message>
-
         <source>Please check your host address</source>
-
         <translation>请核对你的主机地址</translation>
-
     </message>
-
+    <message>
+        <source>Username</source>
+        <translation type="unfinished">用户名</translation>
+    </message>
 </context>
-
 <context>
-
     <name>main</name>
-
     <message>
-
         <source>Transfers</source>
-
         <translation>传输</translation>
-
     </message>
-
     <message>
-
         <source>Add account</source>
-
         <translation>添加账户</translation>
-
     </message>
-
     <message>
-
         <source>About GhostCloud</source>
-
         <translation>关于 GhostCloud</translation>
-
     </message>
-
     <message>
-
         <source>No account available yet. Please add an account to continue.</source>
-
         <translation>无可用账户。请添加账户以继续。</translation>
-
     </message>
-
     <message>
-
         <source>Upload file...</source>
-
         <translation>正在上传文件……</translation>
-
     </message>
-
     <message>
-
         <source>New folder...</source>
-
         <translation>正在新建文件夹……</translation>
-
     </message>
-
 </context>
-
 </TS>
-

10426d9bb71f027c2094116eacb2208f9cb318b6
Alfred Neumayer <dev.beidl@gmail.com>
2020-04-28 12:39:53 +0200

    click: Run with Suru QQC2 styling

--- click/run.sh
+++ click/run.sh
@@ -2,7 +2,7 @@
 
 export LD_LIBRARY_PATH=$PWD/usr/lib:$LD_LIBRARY_PATH
 export QT_QUICK_CONTROLS_MOBILE=true
-export QT_QUICK_CONTROLS_STYLE=Material
+export QT_QUICK_CONTROLS_STYLE=Suru
 
 exec $PWD/usr/bin/harbour-owncloud \
     --desktop_file_hint=${HOME}/.local/share/applications/${APP_ID}.desktop "$@"

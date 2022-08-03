6a39312e5110d378a1aea629468cb2bf575d5761
Alfred Neumayer <dev.beidl@gmail.com>
2022-08-03 20:27:09 +0200

    common: Unfinished UT-based file uploader
    For future developemnts and reference.

---
+++ src/common/src/commands/ubuntutouch/utfileuploadcommandentity.h
@@ -0,0 +1,35 @@
+#ifndef UTFILEUPLOADCOMMANDENTITY_H
+#define UTFILEUPLOADCOMMANDENTITY_H
+
+#include <QObject>
+#include <QFile>
+#include <QDir>
+#include "commandentity.h"
+#include <settings/nextcloudsettingsbase.h>
+#include <ubuntu/download_manager/upload.h>
+#include <ubuntu/download_manager/manager.h>
+
+class UtFileUploadCommandEntity : public CommandEntity
+{
+    Q_OBJECT
+
+public:
+    explicit UtFileUploadCommandEntity(QObject* parent = Q_NULLPTR,
+                                       QString localPath = QStringLiteral(""),
+                                       QString remotePath = QStringLiteral(""),
+                                       AccountBase* settings = Q_NULLPTR);
+
+protected:
+    bool startWork() Q_DECL_OVERRIDE;
+    bool staticProgress() const Q_DECL_OVERRIDE { return false; }
+
+private:
+    Ubuntu::DownloadManager::Manager* m_dlManager = Q_NULLPTR;
+    Ubuntu::DownloadManager::Download* m_download = Q_NULLPTR;
+    AccountBase* m_settings = Q_NULLPTR;
+    bool m_running = false;
+    QFile* m_localFile = Q_NULLPTR;
+    QString m_remotePath;
+};
+
+#endif // UTFILEUPLOADCOMMANDENTITY_H
